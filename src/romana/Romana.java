6dd38dfd56f7f06fc2aa35c818d5cc48c5a82f51
RomanaKlyuka <114306463+RomanaKlyuka@users.noreply.github.com>
2022-12-27 13:19:23 -0600

    Update Romana.java
    Kristina`s and Yaran`s branches were merged

--- src/romana/Romana.java
+++ src/romana/Romana.java
@@ -4,6 +4,9 @@ public class Romana {
 
     public static void main(String[] args) {
         int age =28;
+        
+        // Kristina was added to mater branch.
+        //Yazan was added to master branch 
 
     }
 }
