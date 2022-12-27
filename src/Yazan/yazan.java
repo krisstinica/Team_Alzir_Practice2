cfdc847fd574c6d82b16c6e226faf1ceb439a186
Yazan <yazanhadi0418@gmail.com>
2022-12-26 17:51:35 -0700

    added more lines to yazan

--- src/Yazan/yazan.java
+++ src/Yazan/yazan.java
@@ -8,5 +8,11 @@ public class yazan {
 
         System.out.println("I love coffee");
 
+        System.out.println("----------------------------------------------");
+
+        System.out.println("I have two cats");
+
+        System.out.println("Their name is Bunder and Luna");
+
     }
 }
