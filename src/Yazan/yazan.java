4d0c55746039fcc60db26b4ce20cae6c329cfa6f
Yazan <yazanhadi0418@gmail.com>
2022-12-26 17:50:11 -0700

    created Yazan and added a couple lines

---
+++ src/Yazan/yazan.java
@@ -0,0 +1,12 @@
+package Yazan;
+
+public class yazan {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+
+        System.out.println("I love coffee");
+
+    }
+}
