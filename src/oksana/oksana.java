2bc2e90023fd4d885a7749bc44acc773f8d1f6ae
oksana khanduchka <khanduchkaoxana@gmail.com>
2022-12-28 21:36:51 -0600

    created SDET

---
+++ src/oksana/oksana.java
@@ -0,0 +1,10 @@
+package oksana;
+
+public class oksana {
+
+    public static void main(String[] args) {
+
+        System.out.println("today is good day");
+        System.out.println("all of us will be SDET with salary 120K");
+    }
+}
