ce6623a8fa4275307c79eba66e44677f5f868e8c
kasana <integra1918@gmail.com>
2022-12-28 21:37:12 -0600

    created 1st

---
+++ src/OksanaR/OksanaR.java
@@ -0,0 +1,8 @@
+package OksanaR;
+
+public class OksanaR {
+    public static void main(String[] args) {
+        System.out.println("today is awesome");
+        System.out.println("tomorrow even better");
+    }
+}
